9ae18c3db0b7f60b11d90858540a69d8e6bf3425
ccj_chen <123456>
2022-12-06 23:37:25 +0800

    [add]druid sql解析增加自定义访问者实现

--- src/main/java/com/example/springboot_project/util/DruidSqlParseUtil.java
+++ src/main/java/com/example/springboot_project/util/DruidSqlParseUtil.java
@@ -5,12 +5,14 @@ import com.alibaba.druid.sql.ast.SQLExpr;
 import com.alibaba.druid.sql.ast.SQLStatement;
 import com.alibaba.druid.sql.ast.statement.SQLExprTableSource;
 import com.alibaba.druid.sql.ast.statement.SQLSelect;
+import com.alibaba.druid.sql.ast.statement.SQLSelectGroupByClause;
 import com.alibaba.druid.sql.ast.statement.SQLSelectQuery;
 import com.alibaba.druid.sql.ast.statement.SQLSelectQueryBlock;
 import com.alibaba.druid.sql.ast.statement.SQLSelectStatement;
 import com.alibaba.druid.sql.ast.statement.SQLTableSource;
 import com.alibaba.druid.sql.ast.statement.SQLUnionQuery;
 import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlSchemaStatVisitor;
+import com.alibaba.druid.sql.visitor.SQLASTVisitorAdapter;
 
 import java.sql.SQLSyntaxErrorException;
 import java.util.List;
@@ -25,6 +27,11 @@ import java.util.List;
  * https://github.com/alibaba/druid/wiki/Druid_SQL_AST
  * https://github.com/alibaba/druid/wiki/SQL_Parser_Demo_visitor
  *
+ * druid sql 访问者模式参考文章
+ * https://blog.csdn.net/qq_25104587/article/details/105757507
+ * https://www.cnblogs.com/bigstrong/p/15190473.html
+ * https://www.modb.pro/db/169904
+ *
  * @author canjiechen
  * @version 2.0.0
  * @date 2022/11/28 11:28
@@ -32,7 +39,8 @@ import java.util.List;
 public class DruidSqlParseUtil {
 
     public static void main(String[] args) throws SQLSyntaxErrorException {
-        testVisitor();
+        testCustomVisitor();
+        // testVisitor();
     }
 
     public static void test1() {
@@ -83,11 +91,41 @@ public class DruidSqlParseUtil {
     }
 
     public static void testVisitor() {
-        String sql = "select id, name from A left join B on A.id = B.id";
+        // String sql = "select id, name from A left join B on A.id = B.id";
+        String sql = "select id from A where id = 1";
         String dbType = "mysql";
         SQLSelectStatement statement = (SQLSelectStatement) SQLUtils.parseStatements(sql, dbType).get(0);
         MySqlSchemaStatVisitor visitor = new MySqlSchemaStatVisitor();
         statement.accept(visitor);
         System.out.println(visitor.getTables().toString());
+    }
+
+    public static void testCustomVisitor() {
+        SQLGroupbyVisitor visitor = new SQLGroupbyVisitor();
+        String sql = "select sum(id),name from t_order group by name";
+        String dbType = "mysql";
+        SQLStatement sqlStatement = SQLUtils.parseStatements(sql, dbType).get(0);
+        sqlStatement.accept(visitor);
+        boolean flag = visitor.isHasGroupby();
+        System.out.println(flag);
+    }
+}
+
+
+/***
+ * 自定义访问者类
+ */
+class SQLGroupbyVisitor extends SQLASTVisitorAdapter {
+
+    private boolean hasGroupby = false;
+
+    @Override
+    public boolean visit(SQLSelectGroupByClause x) {
+        this.hasGroupby = true;
+        return false;
+    }
+
+    public boolean isHasGroupby() {
+        return hasGroupby;
     }
 }
